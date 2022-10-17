cef9d101d913209c84782a84225ac88bbad2eb47
Ochieng Brian <68417924+ochiengbrian@users.noreply.github.com>
2022-10-17 18:10:52 +0300

    Add files via upload

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char q;
+
+	q = 'a';
+
+	while (q <= 'z')
+{
+
+	putchar(q);
+
+	q++;
+}
+
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is task 1 if else statement
